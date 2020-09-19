8106e317acb7e55602c9aa19ed1dffe806941e45
onekodate <56372484+onekodate@users.noreply.github.com>
2020-09-19 15:28:35 +0900

    Update mastodon.js

--- mastodon.js
+++ mastodon.js
@@ -58,43 +58,41 @@ function openclose(btn){
     return false;
 }
 
+var posts={}, boosts={}, replies={};
 function loadPage(){
     loading(1)
     //変数割り当て
-    var posts=[], boosts=[], datetimelist=[], datecount={}, datelabel=[], datelist=[], timelist=[];
+    var datetimelist=[], datecount={}, datelabel=[], datelist=[], timelist=[];
     var timecount=new Array(24*60), timelabel=new Array(24*60);
     for(let i=0;i<timecount.length;i++){
         timecount[i]=0;
-        let k = null;
-        if(i%60<10) k='0'+i%60;
-        else k=i%60; 
-        timelabel[i]=(i-i%60)/60+":"+k;
+        timelabel[i]=(i-i%60)/60+":"+set2fig(i%60);
     }
     timelabel.push('24:00');
-    var boost_counter=0, private_counter=0, unlisted_counter=0, direct_counter=0, public_counter=0, reply_counter=0;
+    var boost_counter=0, private_counter=0, unlisted_counter=0, direct_counter=0, public_counter=0;
     //outbox読み取り
     for (const cont of outbox_json["orderedItems"]){
         let date=new Date(cont['published']);
         date.setDate(date.getHours()+9);
-        const time = date.getHours()*60+date.getMinutes();
-        timelist.push(date.getHours()+"Hours");
-        date = date.getFullYear()+"-"+(date.getMonth()+1)+"-"+date.getDate();
+        const timenum = date.getHours()*60+date.getMinutes();
+        timecount[timenum]++;
+        timelist.push(set2fig(date.getHours())+":00-"+set2fig(date.getHours()+1)+":00");
+        const time = set2fig(date.getHours())+":"+set2fig(date.getMinutes())+":"+set2fig(date.getSeconds())+":"+date.getMilliseconds();
+        date = date.getFullYear()+"-"+set2fig(date.getMonth()+1)+"-"+set2fig(date.getDate());
         datelist.push(date);
-        date+="T00:00:00";
-        timecount[time]++;
         if(!datecount[date]) datecount[date]=0;
         datecount[date]++;
         let datetime={};
         datetime.X=date;
-        datetime.Y=time; 
+        datetime.Y=timenum; 
         datetimelist.push(datetime);
 
         if(cont['type']==='Announce'){
+            boosts[date+" "+time]=cont['object'];
             boost_counter++;
-            boosts.push(cont['object']);
         }else{
-            posts.push(cont['object']['content'].replace(/<("[^"]*"|'[^']*'|[^'">])*>/g,''));
-            if(cont['object']['inReplyTo']) reply_counter++;
+            posts[date+" "+time]=cont['object']['content'].replace(/<("[^"]*"|'[^']*'|[^'">])*>/g,'');
+            if(cont['object']['inReplyTo']) replies[date+" "+time]=cont['object']['inReplyTo'];;
             if(cont['cc'].length===0){
                 if(cont['to'].length!==0){
                 if(cont['to'][0].indexOf('followers')===-1) direct_counter++;
@@ -105,13 +103,13 @@ function loadPage(){
         }
     }
     //summary
-    elemid("sumpostcounter").innerText=posts.length;    
+    elemid("sumpostcounter").innerText=public_counter+unlisted_counter+private_counter+direct_counter;    
     elemid("publiccounter").innerText=public_counter;
     elemid("unlistedcounter").innerText=unlisted_counter;
     elemid("privatecounter").innerText=private_counter;
     elemid("directcounter").innerText=direct_counter;
     elemid("boostcounter").innerText=boost_counter;
-    elemid("replycounter").innerText=reply_counter;
+    elemid("replycounter").innerText=Object.keys(replies).length;
     elemid("favcounter").innerText=likes_json['totalItems'];
     var ctx=elemid("circle");
     var myChart = new Chart(ctx,{
@@ -119,14 +117,17 @@ function loadPage(){
         data:{
             datasets:[{
                 data:[public_counter, unlisted_counter, private_counter, direct_counter, boost_counter],
-                backgroundColor:["#5f9ea0","#008080","#2e8b57","#2f4f4f","#483d8b"]
+                backgroundColor:["#ffffff","#d9e1e8","#9baec8","#191b22","#000000"],
+                borderWidth:1,
             }],
             labels:["Public", "Unlisted", "Private", "Direct", "Boost"],
-        }
+        },
+        options:{legend:{display:false,},},
     });
     //ranking
     var replyto=[];
-    for (let obj of posts){
+    for (var key in posts){
+        var obj=posts[key];
         while(obj.indexOf('@')!==-1&&obj.indexOf(' ')!==-1){
             replyto.push(obj.substring(obj.indexOf('@')+1,obj.indexOf(' ')));
             obj=obj.substr(obj.indexOf(' ')+1);
@@ -135,7 +136,8 @@ function loadPage(){
     document.getElementById('replyrank').innerHTML = ranksort(replyto,30);
     
     var boostuser=[]
-    for(const boost of boosts){
+    for(var key in boosts){
+        var boost=boosts[key];
         if(boost.indexOf('users')!==-1&&boost.indexOf('statuses')!==-1){
             boostuser.push(boost.substring(boost.indexOf('users')+6,boost.indexOf('statuses')-1));
         }
@@ -168,12 +170,21 @@ function loadPage(){
         options:{
             scales:{
                 xAxes:[{
-                    scaleLabel:{fontColor:"#ffffff"},
+                    gridLines:{color:"#9baec8",},
                     ticks: {
                         maxTicksLimit:24,
                         maxRotation:0,
-                    }
-                }]
+                        fontColor:"#ffffff",
+                    },
+                }],
+                yAxes:[{
+                    gridLines:{color:"#9baec8",},
+                    ticks: {
+                        maxTicksLimit:24,
+                        maxRotation:0,
+                        fontColor:"#ffffff",
+                    },
+                }],
             },
         }
     });
@@ -191,7 +202,7 @@ function loadPage(){
     elemid("wholedays").innerText=wholedays;
     elemid("daten").innerText=dcs.length;
     elemid("uprate").innerText=Math.round(dcs.length/wholedays*1000)/10;
-    elemid("aver").innerText=Math.round(posts.length/dcs.length);
+    elemid("aver").innerText=Math.round(Object.keys(posts).length/dcs.length);
     elemid("daterank").innerHTML = ranksort(datelist,5);
     elemid("timerank").innerHTML = ranksort(timelist,5);
     /*
@@ -238,38 +249,76 @@ function ranksort(replyto,n){
         }
     }
     for(let i=0;i<n;i++){
-/*
-        if(i===10){
-            replyrank.push('</table>'+
-            '<div class="" id="#readmore5">'+
-            '    <a href="#readmore5" onclick="openclose(this);">Open</a>'+
-            '</div>'+
-            '<div id="readmore5" class="invisible">'+
-            '<table>');
-        }
-*/
         let t=counterlist.indexOf(Math.max.apply(null,counterlist));
         replyrank.push('<tr><th>');
         replyrank.push(i+1+".");
         replyrank.push('</th><th>');
         replyrank.push(namelist[t]);
-        replyrank.push('</th><th>');
+        replyrank.push('</th><th class="right">');
         replyrank.push(counterlist[t]);
         replyrank.push('times');
         replyrank.push('</th></tr>');
         counterlist[t]=0;           
     }
     replyrank.push('</table>');
-/*
-    if(replyrank.length>80){
-        replyrank.push('<p class="readlessbutton">'+
-            '<a href="#readmore5" onclick="openclose(this);">Close</a>'+
-        '</p>'+        
-        '</div>');
-    }
-*/
     return replyrank.join('');
 }
 
-function loadsearch(){
+function loadSearch(){
+    loading(1);
+    var list={}, request=null;
+    if(elemid("word").value){
+        for(var key in posts){
+            if(posts[key].indexOf(elemid("word").value)!==-1){
+                list[key]=posts[key];
+            }
+        }
+        request=elemid("word").value+" from toot content.";
+        elemid("word").value="";
+    }else if(elemid("boost").value){
+        for(var key in boosts){
+            if(boosts[key].indexOf(elemid("boost").value)!==-1){
+                list[key]='<a target="_blank" href='+boosts[key]+">"+boosts[key]+"</a>";
+            }
+        }
+        request=elemid("boost").value+" from boosted user.";
+        elemid("boost").value="";
+    }else if(elemid("reply").value){
+        var replylist={};
+        for(key in replies){
+            if(replies[key].indexOf(elemid("reply").value)!==-1){
+                replylist[replies[key].substr(replies[key].indexOf('statuses')+9,5)]=key;
+            }
+        }
+        for(var key in replylist){
+            list[replylist[key]]='<a target="_blank" href='+replies[replylist[key]]+">"+posts[replylist[key]]+"</a>";
+        }
+        request=elemid("reply").value+" from reply user.";
+        elemid("reply").value="";
+    }else error("You don't input a word.");
+    
+    if(request&&Object.keys(list).length===0){
+        elemid("searchcontent").innerText="No toot was found for your request: "+request;
+        elemid("searchresult").innerText="";
+    }else if(request){
+        var resulttable=['<table>'];
+        for(key in list){
+            resulttable.push('<tr><th class="short">');
+            resulttable.push(key.substr(0,19));
+            resulttable.push('</th><th>');
+            resulttable.push(list[key]);
+            resulttable.push('</th></tr>');   
+        }
+        resulttable.push('</table>');
+        elemid("searchcontent").innerText="You are searching for "+request+" There are "+Object.keys(list).length+" results.";
+        elemid("searchresult").innerHTML=resulttable.join('');
     }
+    
+    loading(0);
+}
+
+function set2fig(num){
+    var ret=num;
+    if(num<10) ret="0"+num;
+    return ret
+}
